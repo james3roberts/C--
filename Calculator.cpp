29a6ab0242075470a5c968aba7fb5fa8a1723837
James Roberts <james3roberts@gmail.com>
2021-05-16 19:16:53 -0600

    Added a no more line.

--- Calculator.cpp
+++ Calculator.cpp
@@ -16,10 +16,10 @@ int  main() // Changed void to int to be able to get numbers.
 	double op1, op2; // changed from int to double to get decimals
 	char operation; // To save the operator
 	char answer = 'Y'; // Changed from double quotes to single quotes, Added a semicolon
-		while (answer == 'y'|| answer == 'Y') // added the upper and lower case y. 
+		while (answer == 'y'|| answer == 'Y') // added the upper and lower case y. 9/13
 		{
 			cout << "Enter expression" << endl; // get expression.
-			cin >> op2 >> operation >> op1; // get the input.
+			cin >> op1 >> operation >> op2; // get the input.
 
 			if (operation == '+')  // Changed from double quotes to single quotes and check to see it the problem is addition 
 			cout << op1 << "+" << op2 << " = " << op1 + op2 << endl; // Print to sum. Changed >> to <<. 
@@ -35,7 +35,13 @@ int  main() // Changed void to int to be able to get numbers.
 
 			cout << "Do you wish to evaluate another expression? " << endl; //Ask if they want another problem.
 			cin >> answer; // save the answer.
-		}
-    cout <<"Program Finished" << endl;// Time to stop. 
+
+            if (answer =='n' || answer =='N')
+		    cout <<"Program Finished" << endl;// Time to stop.
+            
+         }
+         
+
+        
     return 0;  // Added the return statement. 2
 }
